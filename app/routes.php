70e07da7a51a30aa529738d5b3cceae0a93d040c
CDBuckmaster <callumbuckmaster@gmail.com>
2015-07-04 17:05:40 +1000

    Server stuff

---
+++ app/routes.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+Route::get('/butts', function(){
+	if(DB::connection()->getDatabaseName())
+	{
+	   return "conncted sucessfully to database ".DB::connection()->getDatabaseName();
+	}
+
+	return "I dunno";
+});
+
+
